bd7d77f62bbf1148e3335e3078c49eaa58a76266
juste-injuste <79662795+juste-injuste@users.noreply.github.com>
2023-11-22 22:37:15 -0500

    cleanup

--- build/CMakeFiles/Pinakas.dir/cmake_clean.cmake
+++ build/CMakeFiles/Pinakas.dir/cmake_clean.cmake
@@ -1,8 +1,10 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/Pinakas.dir/src/Pinakas.cpp.obj"
-  "CMakeFiles/Pinakas.dir/src/Pinakas.cpp.obj.d"
-  "libPinakas.a"
-  "libPinakas.pdb"
+  "CMakeFiles/Pinakas.dir/example/main.cpp.obj"
+  "CMakeFiles/Pinakas.dir/example/main.cpp.obj.d"
+  "Pinakas.exe"
+  "Pinakas.exe.manifest"
+  "Pinakas.pdb"
+  "libPinakas.dll.a"
 )
 
 # Per-language clean rules from dependency scanning.
